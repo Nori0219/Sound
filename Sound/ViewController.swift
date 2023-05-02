4f6e0daf804fcc3ddec5e4c0d3e87734baceb3d5
NorichikaSugii <wawa60379@gmail.com>
2023-05-02 21:16:47 +0900

    ピアノ、ギター追加

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -13,8 +13,20 @@ class ViewController: UIViewController {
     //ドラムを表示する箱を作る
     @IBOutlet var drumButton: UIButton!
     
+    //ピアノを表示する箱を作る
+    @IBOutlet var pianoButton: UIButton!
+    
+    //ギターを表示する箱を作る
+    @IBOutlet var guitarButton: UIButton!
+    
     //ドラムのサウンドファイルを読み込んで、プレイヤーを作る
     let drumSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
+    
+    //ピアノのサウンドファイルを読み込んで、プレイヤーを作る
+    let pianoSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name: "pianoSound")!.data)
+    
+    //ギターのサウンドファイルを読み込んで、プレイヤーを作る
+    let guitarSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name: "guitarSound")!.data)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +48,40 @@ class ViewController: UIViewController {
     @IBAction func touchUPDrumButton() {
         //ドラムがなっていない画像に切り替える
         drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
+    }
+    
+    //ピアノのタッチが始まった時に呼ばれる
+    @IBAction func touchDownPianoButton() {
+        //ピアノが鳴っている画像に切り替える
+        pianoButton.setImage(UIImage(named: "pianoPlayingImage"), for: .normal)
+        
+        //ピアノの音を鳴らす 連続してタップされた場合に、音源ファイルの再生時間を最初の「０」に戻す
+        pianoSoundPlayer.currentTime = 0
+        
+        //ピアノの音を再生する
+        pianoSoundPlayer.play()
+    }
+    
+    @IBAction func touchUpPianoButton() {
+        //ピアノがなっていない画像に切り替える
+        pianoButton.setImage(UIImage(named: "pianoImage"), for: .normal)
+    }
+    
+    //ギターのタッチが始まった時に呼ばれる
+    @IBAction func touchDownGuitarButton() {
+        //ギターが鳴っている画像に切り替える
+        guitarButton.setImage(UIImage(named: "guitarPlayingImage"), for: .normal)
+        
+        //ギターの音を鳴らす 連続してタップされた場合に、音源ファイルの再生時間を最初の「０」に戻す
+        guitarSoundPlayer.currentTime = 0
+        
+        //ギターの音を再生する
+        guitarSoundPlayer.play()
+    }
+    
+    @IBAction func touchUpGuitarButton() {
+        //ギターがなっていない画像に切り替える
+        guitarButton.setImage(UIImage(named: "guitarImage"), for: .normal)
     }
 
 
